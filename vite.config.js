8831b2d0f9f17594e87776f3fdcfe82528670f67
modernage12 <lorenzoblu@hotmail.it>
2025-08-20 22:12:42 +0200

    Add allowedHosts to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,23 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
-
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-});
+import { defineConfig } from 'vite'; 
+import laravel from 'laravel-vite-plugin'; 
+import vue from '@vitejs/plugin-vue'; 
+ 
+export default defineConfig({ 
+    plugins: [ 
+        laravel({ 
+            input: 'resources/js/app.js', 
+            refresh: true, 
+            preview: { 
+                allowedHosts: ['gdpr-checklist-app.onrender.com'] 
+            }, 
+        }), 
+        vue({ 
+            template: { 
+                transformAssetUrls: { 
+                    base: null, 
+                    includeAbsolute: false, 
+                }, 
+            }, 
+        }), 
+    ], 
+}); 
