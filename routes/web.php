b417f53e22b49587c229b07bb69e824954953952
modernage12 <lorenzoblu@hotmail.it>
2025-08-20 23:50:33 +0200

    Fix routes/web.php and nginx/default.conf

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,29 @@
-<?php
-
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
-});
+<?php  
+  
+use Illuminate\Foundation\Application;  
+use Illuminate\Support\Facades\Route;  
+use Inertia\Inertia;  
+  
+Route::get('/', function () {  
+    return Inertia::render('Welcome', [  
+        'canLogin' => Route::has('login'),  
+        'canRegister' => Route::has('register'),  
+        'laravelVersion' => Application::VERSION,  
+        'phpVersion' => PHP_VERSION,  
+    ]);  
+});  
+  
+Route::middleware([  
+    'auth:sanctum',  
+    config('jetstream.auth_session'),  
+    'verified',  
+])->group(function () {  
+    Route::get('/dashboard', function () {  
+        return Inertia::render('Dashboard');  
+    })->name('dashboard');  
+});  
+  
+// Aggiungo la route catch-all per Inertia.js  
 Route::get('/{any}', function () {  
     return Inertia::render('Welcome');  
-})-, '.*'); 
+})->where('any', '.*'); 
